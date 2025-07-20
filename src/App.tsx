ea2880b1f7e330847ef4adbcc80fe275a69a65db
oliver su <oliversu5251@gmail.com>
2025-07-20 23:25:45 +0800

    feat: add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -101,6 +101,13 @@ function App() {
         <meta property="og:description" content={SEO.desc[lang]} />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content={lang} />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NT2N718QLB"></script>
+        <script>{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-NT2N718QLB');
+        `}</script>
         <script type="application/ld+json">{`
           {
             "@context": "https://schema.org",
